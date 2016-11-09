f93490f3c0f9db124ba243a7a8aab63631ae43e5
stephhappens <stephanie.n.provence@gmail.com>
2016-11-09 17:32:10 -0600

    finish registration page

---
+++ app/Http/Controllers/RegisterController.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const User = use('App/Model/User');
+const Hash = use('Hash');
+
+class RegisterController {
+  //
+  // * index(request, response) {
+  //   //
+  // }
+
+  * create(request, response) {
+    yield response.sendView('user.create');
+  }
+
+  * store(request, response) {
+    const { username, email, password } = request.all();
+
+    try {
+      const user = yield User.create({
+        username,
+        email,
+        password: yield Hash.make(password),
+      });
+
+      yield request.auth.login(user);
+
+      yield request.with({
+        success: 'Ribbit! Your account has been created!',
+      }).flash();
+
+      response.redirect('/');
+    } catch (e) {
+      yield request
+      .withOut('password')
+      .andWith({ error: 'Ribbit! That username or email is already taken.' })
+      .flash();
+
+      response.redirect('back');
+    }
+  }
+
+  // * show(request, response) {
+  //   //
+  // }
+  //
+  // * edit(request, response) {
+  //   //
+  // }
+  //
+  // * update(request, response) {
+  //   //
+  // }
+  //
+  // * destroy(request, response) {
+  //   //
+  // }
+
+}
+
+module.exports = RegisterController;
--- app/Http/routes.js
+++ app/Http/routes.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +15,9 @@
 | Route.resource('user', 'UserController')
 */
 
-const Route = use('Route')
+const Route = use('Route');
 
-Route.on('/').render('welcome')
+Route.on('/').render('layout');
+
+Route.get('/register', 'RegisterController.create');
+Route.post('/register', 'RegisterController.store');
